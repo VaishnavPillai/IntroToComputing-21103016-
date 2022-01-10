68efb8d8f2363077e43626805ae635d13da5e428
VaishnavPillai <97439478+VaishnavPillai@users.noreply.github.com>
2022-01-10 15:34:34 +0530

    Add files via upload

---
+++ question 3.py
@@ -0,0 +1,12 @@
+name=input("Please enter name: ")
+sid= input("Enter student Id: ")
+gender=input("Enter gender: ")
+course_name=input("Enter your course name: ")
+cgpa=input("Enter cgpa: ")
+sid=int(sid)
+cgpa=float(cgpa)
+#our list now name as student_info
+data=['Name','SID','Gender','Course name','Cgpa']
+print(data)
+student_info=[name,sid,gender,course_name,cgpa]
+print(student_info)
