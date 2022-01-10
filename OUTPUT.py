24b1f409b62004b55e0b31d298b47b59a8c953d9
VaishnavPillai <97439478+VaishnavPillai@users.noreply.github.com>
2022-01-10 15:35:14 +0530

    Add files via upload

---
+++ OUTPUT.py
@@ -0,0 +1,44 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:/Users/user/Desktop/project/question 5.py =============
+Please give number 1: 7
+Please give number 2: 8
+Please give number 3: 9
+8.0
+
+============= RESTART: C:/Users/user/Desktop/project/question 5.py =============
+Please enter gross income: 100000
+Enter the no. of dependents: 3
+taxable income:
+81000.0
+tax:
+16200.0
+
+============= RESTART: C:/Users/user/Desktop/project/question 5.py =============
+Please enter name: Vaishnav pillai
+Enter student Id: 21103016
+Enter gender: M
+Enter your course name: CSE
+Enter cgpa: 10.0
+['Name', 'SID', 'Gender', 'Course name', 'Cgpa']
+['Vaishnav pillai', 21103016, 'M', 'CSE', 10.0]
+
+============= RESTART: C:/Users/user/Desktop/project/question 5.py =============
+Enter student 1 marks: 94
+Enter student 2 marks: 92
+Enter student 3 marks: 95
+Enter student 4 marks: 96
+Enter student 5 marks: 97
+List: 
+[94, 92, 95, 96, 97]
+Sorted List(decreasing order): 
+[97, 96, 95, 94, 92]
+
+============= RESTART: C:/Users/user/Desktop/project/question 5.py =============
+(a)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+(b)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
