5c39fce88784d49b4a715cfd07c0aa11a524d987
VaishnavPillai <97439478+VaishnavPillai@users.noreply.github.com>
2022-02-13 12:59:35 +0530

    Add files via upload

---
+++ assignment3_OUTPUT.py
@@ -0,0 +1,116 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/assignment 3.py
+Give a string-arshggjgsra
+{'a': 2, 'r': 2, 's': 2, 'h': 1, 'g': 3, 'j': 1}
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/assignment 3.py
+Give month-12
+Give day-31
+Give year-1800
+Date- 31 / 12 / 1800
+Next Date- 1/1/ 1801
+Done!
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/assignment 3.py
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Done!
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/assignment 3.py
+Give a number between 4 and 10 including them-9
+Performance=Excellent
+Letter Grade=A
+Done!
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/assignment 3.py
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+Done!
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/assignment 3.py
+----------------(a)-------------------
+Give the sid (a number):9
+Enter your name:1
+give a letter y or n:y
+Give the sid (a number):1
+Enter your name:hj
+give a letter y or n:3
+The required dictionary-- {9: '1', 1: 'hj'}
+The list will be used in further parts--
+The required list-- [(9, '1'), (1, 'hj')]
+Done!
+----------------(b)------------------------
+The dictionary we had-
+{9: '1', 1: 'hj'}
+now we have exchanged the key value pair but its not sorted-
+{'1': 9, 'hj': 1}
+The unsorted list--
+[('1', 9), ('hj', 1)]
+The sorted list--
+[('1', 9), ('hj', 1)]
+The sorted dictionary-
+{'1': 9, 'hj': 1}
+This is the dictionary sorted on the basis of name-
+{9: '1', 1: 'hj'}
+Done!
+-----------------(c)-----------------
+The list we made before will be used now-
+[(9, '1'), (1, 'hj')]
+now the list is sorted-
+[(1, 'hj'), (9, '1')]
+sorted dictionary based on sid- {1: 'hj', 9: '1'}
+Done!
+-----------------(d)-----------------
+Please enter one of the sids you entered before-1
+The name of the student with the entered sid is-
+hj
+Done!
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/assignment 3.py
+Give a number-1
+Give a number-2
+[1, 2, 3]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+Give y to contnue and n to stop going further-n
+Now we got a list having a fibonacci series-
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+Average of the sequence-
+34.09090909090909
+Done!
+
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/assignment 3.py
+-------------(a)------------
+{1, 3, 5, 6, 8}
+Done!
+-------------(b)------------
+{3, 6, 8, 9, 13, 17}
+Done!
+-------------(c)------------
+{1, 2, 4, 5}
+Done!
+-------------(d)------------
+{6, 7, 8, 9, 10}
+Done!
+-------------(e)------------
+{10, 7}
+Done!
